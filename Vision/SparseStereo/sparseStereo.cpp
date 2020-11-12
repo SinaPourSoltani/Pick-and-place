c0a34ba6196687a0424eb0b46aedbef19c4b9a5a
JensOHI <jens@pilbro.dk>
2020-11-12 14:57:16 +0100

    Changed the functionality in sparseStereo.cpp to be in a class for itself. Also gets both the images from the left and right camera.

--- Vision/SparseStereo/sparseStereo.cpp
+++ Vision/SparseStereo/sparseStereo.cpp
@@ -30,90 +30,129 @@ using rw::sensor::Image;
 using namespace rwlibs::simulation;
 using namespace rws;
 
+#define LEFT "left"
+#define RIGHT "right"
+
+class SparseStereo{
+  private:
+    WorkCell::Ptr wc;
+    Frame* leftCamera;
+    Frame* rightCamera;
+    PropertyMap leftCameraProperties;
+    PropertyMap rightCameraProperties;
+
+    string scene, leftCam, rightCam;
+
+    double leftFovy, rightFovy;
+    int leftWidth, leftHeight, rightWidth, rightHeight;
+
+  public:
+    SparseStereo(char** argv){
+      wc = WorkCellLoader::Factory::load(argv[1]);
+      if(wc.isNull()){RW_THROW("WorkCell could not be loaded.");}
+      scene = argv[1];
+
+      leftCamera = wc->findFrame(argv[2]);
+      if(leftCamera == nullptr){RW_THROW("Left camera frame could not be found.");}
+      leftCam = argv[2];
+
+      rightCamera = wc->findFrame(argv[3]);
+      if(rightCamera == nullptr){RW_THROW("Right camera frame could not be found.");}
+      rightCam = argv[3];
+
+      leftCameraProperties = leftCamera->getPropertyMap();
+      if (!leftCameraProperties.has("Camera")){RW_THROW("Left camera frame does not have Camera property.");}
+
+      rightCameraProperties = rightCamera->getPropertyMap();
+      if (!rightCameraProperties.has("Camera")){RW_THROW("Right camera frame does not have Camera property.");}
+
+      //Printing the properties of the camera
+      const string leftCameraParameters = leftCameraProperties.get<string>("Camera");
+      istringstream iss (leftCameraParameters, istringstream::in);
+      iss >> leftFovy >> leftWidth >> leftHeight;
+      cout << "Left camera properties:  fov " << leftFovy << " width " << leftWidth << " height " << leftHeight << endl;
+      const string rightCameraParameters = rightCameraProperties.get<string>("Camera");
+      iss = istringstream(rightCameraParameters, istringstream::in);
+      iss >> rightFovy >> rightWidth >> rightHeight;
+      cout << "Right camera properties: fov " << rightFovy << " width " << rightWidth << " height " << rightHeight << endl;
+    }
+
+    void takeImages(){
+      RobWorkStudioApp app("");
+      app.start();
+
+      RobWorkStudio* const rwstudio = app.getRobWorkStudio();
+      rwstudio->postOpenWorkCell(scene);
+      TimerUtil::sleepMs(5000);
+
+      //Setting up for leftCamera
+      const SceneViewer::Ptr leftGldrawer = rwstudio->getView()->getSceneViewer();
+      const GLFrameGrabber::Ptr leftFramegrabber = ownedPtr(new GLFrameGrabber(leftWidth,leftHeight,leftFovy));
+      leftFramegrabber->init(leftGldrawer);
+      SimulatedCamera::Ptr leftSimcam = ownedPtr(new SimulatedCamera("SimulatedCamera", leftFovy, leftCamera, leftFramegrabber));
+      leftSimcam->setFrameRate(100);
+      leftSimcam->initialize();
+      leftSimcam->start();
+      leftSimcam->acquire();
+
+
+      static const double DT = 0.001;
+      const Simulator::UpdateInfo leftInfo(DT);
+      State state = wc->getDefaultState();
+      int cnt = 0;
+      const Image* img;
+      while(!leftSimcam->isImageReady()){
+        cout << "Image is not ready yet. Iteration " << cnt << endl;
+        leftSimcam->update(leftInfo,state);
+        cnt++;
+      }
+      img = leftSimcam->getImage();
+      img->saveAsPPM("Images/left_image.ppm");
+      cout << "Took " << cnt << " steps" << endl;
+      cout << "Left image: " << img->getWidth() << "x" << img->getHeight() << endl;
+      leftSimcam->stop();
+
+      //Setting up for rightCamera
+      const SceneViewer::Ptr rightGldrawer = rwstudio->getView()->getSceneViewer();
+      const GLFrameGrabber::Ptr rightFramegrabber = ownedPtr(new GLFrameGrabber(rightWidth,rightHeight,rightFovy));
+      rightFramegrabber->init(rightGldrawer);
+      SimulatedCamera::Ptr rightSimcam = ownedPtr(new SimulatedCamera("SimulatedCamera", rightFovy, rightCamera, rightFramegrabber));
+      rightSimcam->setFrameRate(100);
+      rightSimcam->initialize();
+      rightSimcam->start();
+      rightSimcam->acquire();
+
+      const Simulator::UpdateInfo rightInfo(DT);
+      cnt = 0;
+      while(!rightSimcam->isImageReady()){
+        cout << "Image is not ready yet. Iteration " << cnt << endl;
+        rightSimcam->update(rightInfo,state);
+        cnt++;
+      }
+      img = rightSimcam->getImage();
+      img->saveAsPPM("Images/right_image.ppm");
+      cout << "Took " << cnt << " steps" << endl;
+      cout << "Right image: " << img->getWidth() << "x" << img->getHeight() << endl;
+      rightSimcam->stop();
+
+      app.close();
+    }
+
+    //virtual ~SparseStereo();
+};
+
+
 
 int main(int argc, char** argv) {
 
   //How to call the script
-  if(argc < 3){
-    cout << "Usage: " << argv[0] << " <scene> <camera>" << endl;
+  if(argc < 4){
+    cout << "Usage: " << argv[0] << " <scene> <left camera> <right camera>" << endl;
     return 0;
   }
-  //Loading workcell
-  const WorkCell::Ptr wc = WorkCellLoader::Factory::load(argv[1]);
-  if(wc.isNull()){
-    RW_THROW("WorkCell could not be loaded.");
-  }
-  //Loading camera frame
-  Frame* const camera = wc->findFrame(argv[2]);
-  if(camera == nullptr){
-    RW_THROW("Camera frame could not be found.");
-  }
-  //Loading properties of camera
-  const PropertyMap& properties = camera->getPropertyMap();
-  if (!properties.has("Camera")){
-    RW_THROW("Camera frame does not have Camera property.");
-  }
-  //Printing the properties of the camera
-  const string parameters = properties.get<string>("Camera");
-  istringstream iss (parameters, istringstream::in);
-  double fovy, width, height;
-  iss >> fovy >> width >> height;
-  cout << "Camera properties: fov " << fovy << " width " << width << " height " << height << endl;
 
-
-  RobWorkStudioApp app("");
-  app.start();
-
-  RobWorkStudio* const rwstudio = app.getRobWorkStudio();
-  rwstudio->postOpenWorkCell(argv[1]);
-  TimerUtil::sleepMs(5000);
-
-
-  const SceneViewer::Ptr gldrawer = rwstudio->getView()->getSceneViewer();
-  const GLFrameGrabber::Ptr framegrabber = ownedPtr(new GLFrameGrabber(width,height,fovy));
-  framegrabber->init(gldrawer);
-  SimulatedCamera::Ptr simcam = ownedPtr(new SimulatedCamera("SimulatedCamera", fovy, camera, framegrabber));
-  simcam->setFrameRate(100);
-  simcam->initialize();
-  simcam->start();
-  simcam->acquire();
-
-
-  static const double DT = 0.001;
-  const Simulator::UpdateInfo info(DT);
-  State state = wc->getDefaultState();
-  int cnt = 0;
-  const Image* img;
-  while(!simcam->isImageReady()){
-    cout << "Image is not ready yet. Iteration " << cnt << endl;
-    simcam->update(info,state);
-    cnt++;
-  }
-  img = simcam->getImage();
-  img->saveAsPPM("Image.ppm");
-  cout << "Took " << cnt << " steps" << endl;
-  cout << "Image: " << img->getWidth() << "x" << img->getHeight() << endl;
-
-  simcam->stop();
-  app.close();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  SparseStereo sparseStereo(argv);
+  sparseStereo.takeImages();
 
 
 
