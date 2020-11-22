eea37902ca15b88e7b992b4af2d34f8dd19938f9
Sina Pour Soltani <sinapsoltani@gmail.com>
2020-11-22 23:27:33 +0100

    Implement reachability analysis for specified object.

---
+++ Robotics/build/CMakeFiles/graspAnalysis.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Project/Pick-and-place/Robotics/reachability.cpp" "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Project/Pick-and-place/Robotics/build/CMakeFiles/graspAnalysis.dir/reachability.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWork/cmake/../src"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/usr/local/include"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWork/cmake/../ext/rwyaobi"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWork/cmake/../ext/rwpqp"
+  "/usr/local/include/lua"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWork/cmake/../ext/qhull/src"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWork/cmake/../ext/csgjs/src"
+  "/usr/local/Cellar/fcl/0.6.1/include"
+  "/usr/local/Frameworks/Python.framework/Versions/3.8/include/python3.8"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWorkStudio/cmake/../src"
+  "/Users/sinapoursoltani/Documents/Syddansk Universitet/Kandidat/1. Semester/RoVi/Robotics/RobWork/RobWorkStudio/cmake/../ext/qtpropertybrowser/src"
+  "/usr/local/opt/qt5/lib/QtCore.framework"
+  "/usr/local/opt/qt5/lib/QtCore.framework/Headers"
+  "/usr/local/opt/qt5/./mkspecs/macx-clang"
+  "/usr/local/opt/qt5/lib/QtGui.framework"
+  "/usr/local/opt/qt5/lib/QtGui.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/usr/local/opt/qt5/lib/QtWidgets.framework"
+  "/usr/local/opt/qt5/lib/QtWidgets.framework/Headers"
+  "/usr/local/opt/qt5/lib/QtOpenGL.framework"
+  "/usr/local/opt/qt5/lib/QtOpenGL.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Robotics/reachability.cpp
+++ Robotics/reachability.cpp
@@ -11,21 +11,23 @@
 #include <rw/invkin.hpp>
 #include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
 
-
 #include <iostream>
 #include <string>
 
 USE_ROBWORK_NAMESPACE
 using namespace std;
 using namespace robwork;
+using namespace rw::math;
+using namespace rw::models;
+using namespace rw::kinematics;
 
-std::vector<rw::math::Q> getConfigurations(const std::string nameGoal, const std::string nameTcp, rw::models::SerialDevice::Ptr robot, rw::models::WorkCell::Ptr wc, rw::kinematics::State state)
+vector<Q> getConfigurations(const string nameGoal, const string nameTcp, rw::models::SerialDevice::Ptr robot, rw::models::WorkCell::Ptr wc, rw::kinematics::State state)
 {
     // Get, make and print name of frames
-    const std::string robotName = robot->getName();
-    const std::string nameRobotBase = robotName + "." + "Base";
-    const std::string nameRobotTcp = robotName + "." + "TCP";
-
+    const string robotName = robot->getName();
+    const string nameRobotBase = robotName + "." + "Base";
+    const string nameRobotTcp = robotName + "." + "TCP";
+    
     // Find frames and check for existence
     rw::kinematics::Frame* frameGoal = wc->findFrame(nameGoal);
     rw::kinematics::Frame* frameTcp = wc->findFrame(nameTcp);
@@ -39,16 +41,46 @@ std::vector<rw::math::Q> getConfigurations(const std::string nameGoal, const std
         std::cout << " Found \"" << nameRobotBase << "\": " << (frameRobotBase==NULL ? "NO!" : "YES!") << std::endl;
         std::cout << " Found \"" << nameRobotTcp << "\": " << (frameRobotTcp==NULL ? "NO!" : "YES!") << std::endl;
     }
-
+    
     // Make "helper" transformations
     rw::math::Transform3D<> frameBaseTGoal = rw::kinematics::Kinematics::frameTframe(frameRobotBase, frameGoal, state);
     rw::math::Transform3D<> frameTcpTRobotTcp = rw::kinematics::Kinematics::frameTframe(frameTcp, frameRobotTcp, state);
-
+    
     // get grasp frame in robot tool frame
     rw::math::Transform3D<> targetAt = frameBaseTGoal * frameTcpTRobotTcp;
-
+    
     rw::invkin::ClosedFormIKSolverUR::Ptr closedFormSovler = rw::common::ownedPtr( new rw::invkin::ClosedFormIKSolverUR(robot, state) );
     return closedFormSovler->solve(targetAt, state);
+}
+
+#define MIN_Y_WORKAREA -0.525
+#define MAX_Y_WORKAREA 0.175
+#define MIN_X_WORKAREA -0.325
+#define MAX_X_WORKAREA 0.325
+
+#define MAX_X_PLACEAREA 0.125
+#define MAX_Y_PLACEAREA -0.325
+
+#define STEP_RES 3
+#define STEP_SIZE_X (MAX_X_WORKAREA - MIN_X_WORKAREA) / STEP_RES
+#define STEP_SIZE_Y (MAX_Y_WORKAREA - MIN_Y_WORKAREA) / STEP_RES
+
+bool isInsideWorkArea(double x, double y){
+    return !(x > MAX_X_PLACEAREA && y < MAX_Y_PLACEAREA);
+}
+
+vector<Vector3D<>> getBaseFramePositions(){
+    vector<Vector3D<>> base_positions;
+    
+    for (double y=MIN_Y_WORKAREA; y <= MAX_Y_WORKAREA; y += STEP_SIZE_Y) {
+        for (double x=MIN_X_WORKAREA; x <= MAX_X_WORKAREA; x += STEP_SIZE_X) {
+            if (isInsideWorkArea(x,y)) {
+                Vector3D<> pos(x, y, 0.01);
+                base_positions.push_back(pos);
+            }
+        }
+    }
+    return base_positions;
 }
 
 
@@ -58,114 +90,117 @@ int main(int argc, char** argv)
     // how to call the script
     if(argc < 4)
     {
-        cout << "Usage: " << argv[0] << " <scene> <robot> <object>" << endl;
+        cout << "Usage: " << argv[0] << " <scene> <object> <path/to/save/rw.play>" << endl;
         return 0;
     }
-
+    
+    string scene = string(argv[1]);
+    string object = string(argv[2]);
+    string rwplayPath = string(argv[3]);
+    
     // load workcell
-    rw::models::WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(argv[1]);
-    if(wc == NULL){
+    WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(scene);
+    if(wc == NULL) {
         RW_THROW("COULD NOT LOAD scene... check path!");
         return -1;
     }
-
-    // finding serial device
-    rw::models::SerialDevice::Ptr robot = wc->findDevice<rw::models::SerialDevice>(argv[2]);
-    if(robot==NULL)
-    {
+    
+    // find robot base frame
+    MovableFrame::Ptr base_frame = wc->findFrame<MovableFrame>("URReference");
+    if(base_frame==NULL) {
+        RW_THROW("COULD not find robot base... check model");
+        return -1;
+    }
+    
+    // find serial device
+    SerialDevice::Ptr robot = wc->findDevice<SerialDevice>("UR-6-85-5-A");
+    if(robot==NULL) {
         RW_THROW("COULD not find device robot... check model");
         return -1;
     }
-
+    
     // find relevant frames
-    rw::kinematics::MovableFrame::Ptr objectFrame = wc->findFrame<rw::kinematics::MovableFrame>(argv[3]);
-    if(objectFrame==NULL)
-    {
+    MovableFrame::Ptr objectFrame = wc->findFrame<MovableFrame>(object);
+    if(objectFrame==NULL) {
         RW_THROW("COULD not find movable object frame... check model");
         return -1;
     }
-
+    
+    string graspTarget = "GraspTarget" + object;
+    MovableFrame::Ptr objectGraspFrame = wc->findFrame<MovableFrame>(graspTarget);
+    if(objectGraspFrame==NULL) {
+        RW_THROW("COULD not find movable objectGrasp frame... check model");
+        return -1;
+    }
+    
     //Getting Table frames
-    rw::kinematics::FixedFrame::Ptr tableFrame = wc->findFrame<rw::kinematics::FixedFrame>("Table");
-    if(tableFrame==NULL)
-    {
+    FixedFrame::Ptr tableFrame = wc->findFrame<FixedFrame>("Table");
+    if(tableFrame==NULL) {
         RW_THROW("COULD not find Table frame... check model");
         return -1;
     }
-
-
+    
+    
     // setup collision detector
     rw::proximity::CollisionDetector::Ptr detector = rw::common::ownedPtr(new rw::proximity::CollisionDetector(wc, rwlibs::proximitystrategies::ProximityStrategyFactory::makeDefaultCollisionStrategy()));
-
-
+    
     State state = wc->getDefaultState();
     vector<rw::math::Q> collisionFreeSolutions;
-
-    rw::math::Rotation3D<> objectRotInv = inverse(objectFrame->getTransform(state).R());
-
-
-    for(double rollAngle = 0; rollAngle < 360.0; rollAngle += 5.0)
-    {
-	      cout << "RollAngle: " << rollAngle << endl;
-
-
-
-        //Ved 180 går den under objektet.
-        for(double pitchAngle = 0; pitchAngle < 180.0; pitchAngle += 5.0) // TODO check if 180 is enough
+    
+    Rotation3D<> objectRotInv = inverse(objectFrame->getTransform(state).R());
+    
+    TimedStatePath tStatePath;
+    double time = 0;
+    
+    vector<Vector3D<>> base_positions = getBaseFramePositions();
+    Rotation3D<> base_rot = base_frame->getTransform(state).R();
+    
+    for(auto base_pos : base_positions) {
+        cout << "Base: " << base_pos << endl;
+        base_frame->moveTo(Transform3D<>(base_pos, base_rot),state);
+        
+        for(double rollAngle = 0; rollAngle <= 180.0; rollAngle += 10.0)
         {
-
-
-
-
-            objectFrame->moveTo(
-              rw::math::Transform3D<>(
-                rw::math::Vector3D<>(tableFrame->getTransform(state).P() + objectFrame->getTransform(state).P()),
-                (rw::math::RPY<>(0,(180+5.0)*rw::math::Deg2Rad,-90*rw::math::Deg2Rad).toRotation3D()) *
-                 (rw::math::RPY<>(rollAngle*rw::math::Deg2Rad,pitchAngle*rw::math::Deg2Rad,0).toRotation3D())
-                )
-              , state);
-
-            /*
-            cout << "Transform3D: " << endl << rw::math::Transform3D<>(
-              rw::math::Vector3D<>(tableFrame->getTransform(state).P() + objectFrame->getTransform(state).P()),
-              (rw::math::RPY<>(0,180*rw::math::Deg2Rad,90*rw::math::Deg2Rad).toRotation3D()) *
-               (rw::math::RPY<>(pitchAngle*rw::math::Deg2Rad,rollAngle*rw::math::Deg2Rad,0).toRotation3D())
-             ).R() << endl;
-
-            cout << "Rot:" << endl << (rw::math::RPY<>(0,180*rw::math::Deg2Rad,90*rw::math::Deg2Rad).toRotation3D()) << endl;
-            cout << "Rot_reach:" << endl << rw::math::RPY<>(pitchAngle*rw::math::Deg2Rad,rollAngle*rw::math::Deg2Rad,0).toRotation3D() << endl;
-            */
-
-
-            string graspTarget = string(argv[3]) + "GraspTarget";
-            vector<rw::math::Q> solutions = getConfigurations(graspTarget, "GraspTCP",robot, wc, state);
-
-            for(unsigned int i=0; i<solutions.size(); i++){
-                // set the robot in that configuration and check if it is in collision
-                robot->setQ(solutions[i], state);
-                if( !detector->inCollision(state,NULL,true) ){
-                    collisionFreeSolutions.push_back(solutions[i]); // save it
-                    break; // we only need one
+            cout << "RollAngle: " << rollAngle << endl;
+            
+            for(double pitchAngle = 90; pitchAngle <= 270; pitchAngle += 5.0)
+            {
+                
+                objectGraspFrame->moveTo(Transform3D<>(
+                                                       Vector3D<>(
+                                                                  tableFrame->getTransform(state).P() + objectFrame->getTransform(state).P()),
+                                                       RPY<>(rollAngle * Deg2Rad, 0, pitchAngle * Deg2Rad).toRotation3D()
+                                                       )
+                                         , state);
+                
+                vector<rw::math::Q> solutions = getConfigurations(graspTarget, "GraspTCP",robot, wc, state);
+                
+                for(unsigned int i=0; i<solutions.size(); i++){
+                    // set the robot in that configuration and check if it is in collision
+                    robot->setQ(solutions[i], state);
+                    if( !detector->inCollision(state,NULL,true) ){
+                        
+                        collisionFreeSolutions.push_back(solutions[i]); // save it
+                        
+                        robot->setQ(solutions[i], state);
+                        tStatePath.push_back(TimedState(time,state));
+                        time += 0.01;
+                        
+                        //break; // we only need one
+                    }
                 }
             }
         }
     }
-
+    
+    
     cout << "Current position of the robot vs object to be grasped has: "
     << collisionFreeSolutions.size()
     << " collision-free inverse kinematics solutions!" << std::endl;
-
+    
     // visualize them
-    TimedStatePath tStatePath;
-    double time=0;
-    for(unsigned int i=0; i<collisionFreeSolutions.size(); i++){
-        robot->setQ(collisionFreeSolutions[i], state);
-        tStatePath.push_back(TimedState(time,state));
-        time+=0.01;
-    }
-
-    rw::loaders::PathLoader::storeTimedStatePath(*wc, tStatePath, "../Project_WorkCell/visu.rwplay");
-
+    rw::loaders::PathLoader::storeTimedStatePath(*wc, tStatePath, rwplayPath);
+    
     return 0;
-
+    
 }
