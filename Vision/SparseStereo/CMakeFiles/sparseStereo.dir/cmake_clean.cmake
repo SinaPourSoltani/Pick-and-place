e2c14c95adafc455ea94778075c6b9be5c86741e
Jens Otto Hee Iversen <71442812+JensOHI@users.noreply.github.com>
2020-12-03 14:49:15 +0100

    Sparse stereo (#1)
    * Adds the finalization of the sparse stereo, except for the feature detection. The triangulation works, noise is add to a RGB image and the cmake file has been changed.
    
    * Adds the feature detection for the triangulation. It returns three points in 3D space, one for each cylinder. It haven't been test properly with the newest scene so the points in 3D space haven't been check if they are correct.

--- Vision/SparseStereo/CMakeFiles/sparseStereo.dir/cmake_clean.cmake
+++ Vision/SparseStereo/CMakeFiles/sparseStereo.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/sparseStereo.dir/sparseStereo.cpp.o"
-  "bin/sparseStereo.pdb"
-  "bin/sparseStereo"
+  "sparseStereo.pdb"
+  "sparseStereo"
 )
 
 # Per-language clean rules from dependency scanning.
