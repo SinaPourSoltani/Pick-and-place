5bb04961eef05dfda8ee149735bf9496f11ae5c1
JensOHI <jens@pilbro.dk>
2020-12-08 02:54:34 +0100

    Adds the so its possible to run P2P, P2PB and RRT. Also adds so it is saved in a .rwplay file.

--- PickNPlace/pickAndPlace.cpp
+++ PickNPlace/pickAndPlace.cpp
@@ -34,10 +34,10 @@ int main(int argc, char** argv){
         cout << "Usage: " << argv[0] << " <scene> [motion planning method]" << endl;
         return 0;
     }
-    
+
     string scene = string(argv[1]);
     string mpm = P2P;
-    
+
     if(argc > 2){
         string input = string(argv[2]);
         if(input == P2P || input == P2PB || input == RRT){
@@ -46,28 +46,28 @@ int main(int argc, char** argv){
             throw("Undefined Motion Planning Methods [default: P2P] :\n Choose between:\n - P2P\n - P2PB\n - RRT\n");
         }
     }
-    
+
     string trajectoryFilePath = "traj.dat";
     string qpathFilePath = "qconfig.rwplay";
-    
+
     // load workcell
     WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(scene);
     if(wc == NULL) {
         RW_THROW("COULD NOT LOAD scene... check path!");
         return -1;
     }
-    
+
     // find serial device
     SerialDevice::Ptr robot = wc->findDevice<SerialDevice>("UR-6-85-5-A");
     if(robot==NULL) {
         RW_THROW("COULD not find device robot... check model");
         return -1;
     }
-    
+
     Math::seed();
-    
+
     // Predefined place locations
-    
+
     Transform3D<> placeRed(Vector3D<>(PLACE_CENTER_X,
                                       PLACE_CENTER_Y,
                                       0.191),
@@ -81,7 +81,7 @@ int main(int argc, char** argv){
                                        placeGreen.P()(2) + CYLG_HEIGHT / 2 + CYLB_HEIGHT / 2 + CYL_OFFSET),
                             RPY<>(0,Deg2Rad * 180,0));
     vector<Transform3D<> > places = {placeRed, placeGreen, placeBlue};
-    
+
     // Vision determined pick locations
     vector<string> objectNames = {"CylinderRed", "CylinderGreen", "CylinderBlue"};
     vector<Transform3D<> > picks = {
@@ -89,7 +89,7 @@ int main(int argc, char** argv){
         Transform3D<>(Vector3D<>(0, 0.474, 0.150), RPY<>(0,Deg2Rad * 180,0)),
         Transform3D<>(Vector3D<>(-0.25, 0.474, 0.170), RPY<>(0,Deg2Rad * 180,0)),
     };
-    
+
     #if D2D
         SparseStereo ss(scene);
         ss.addNoiseToImages(0,0.1);
@@ -105,19 +105,24 @@ int main(int argc, char** argv){
         ds.visualizePointClouds();
         // set pick points
     #endif
-    
+
     State state = wc->getDefaultState();
     MotionPlanner mp(wc, robot, state);
     vector<float> timeBetweenPoints = {1, 1, 1, 1, 1, 1, 1, 1};
     vector<float> blendFractions = {0.0, 0.5, 0.0, 0.5, 0.5, 0.5, 0.0, 0.5};
-    
+
     if(mpm == RRT){
+        TimedStatePath tStatePaths;
         for(unsigned int i = 0; i < picks.size(); i++){
             mp.setPickAndPlace(picks[i], objectNames[i], places[i]);
             vector<QPath> qpaths = mp.RRTInterpolate(0.05);
-            mp.writeQPathVectorToFile(qpaths, qpathFilePath);
+            TimedStatePath tStatePath = mp.writeQPathVectorToFile(qpaths);
+            for(unsigned int p = 0; p < tStatePath.size(); p++){
+              tStatePaths.push_back(tStatePath[p]);
+            }
         }
-        
+        mp.writeTimedStatePath(tStatePaths, qpathFilePath);
+
     } else if(mpm == P2PB) {
         for(unsigned int i = 0; i < picks.size(); i++){
             mp.setPickAndPlace(picks[i], objectNames[i], places[i]);
@@ -127,12 +132,19 @@ int main(int argc, char** argv){
             mp.writeQPathToFile(path, qpathFilePath);
         }
     } else {
+        TimedStatePath tStatePaths;
+        InterpolatorTrajectory<Transform3D<> > trajectories;
         for(unsigned int i = 0; i < picks.size(); i++){
             mp.setPickAndPlace(picks[i], objectNames[i], places[i]);
             InterpolatorTrajectory<Transform3D<> > traj = mp.linearlyInterpolate(timeBetweenPoints);
+            trajectories.add(&traj);
             QPath path = mp.trajectoryToQPath(traj);
-            mp.writeTrajectoryToFile(traj, trajectoryFilePath);
-            mp.writeQPathToFile(path, qpathFilePath);
+            TimedStatePath tStatePath = mp.writeQPathToFile(path, qpathFilePath);
+            for(unsigned int p = 0; p < tStatePath.size(); p++){
+              tStatePaths.push_back(tStatePath[p]);
+            }
         }
+        mp.writeTrajectoryToFile(trajectories, trajectoryFilePath);
+        mp.writeTimedStatePath(tStatePaths, qpathFilePath);
     }
 }
